7b944f1330b4390329bbcaa6f1f85a8eccc2b74e
Chu-An, Huang <dreamcastle.andy@gmail.com>
2019-03-11 11:55:23 +0800

    [Modify] source_site = 2

--- newmagzinecrawler.py
+++ newmagzinecrawler.py
@@ -2,7 +2,9 @@ from travelspider import TravelSpider
 
 class NewmagzineCrawler(TravelSpider):
     """docstring for NewmagzineCrawler."""
-    pass
+    def __init__(self, base_url):
+        super(NewmagzineCrawler, self).__init__(self, base_url)
+        self.source_site = 2
 
 def main():
     crawler = NewmagzineCrawler(base_url="https://www.newamazing.com.tw")
