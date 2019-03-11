fc8904be30b2f1191107be6c40b5560bfc4e65f9
Chu-An, Huang <dreamcastle.andy@gmail.com>
2019-03-11 11:56:56 +0800

    [Modify] source_site = 3

--- gloriacrawler.py
+++ gloriacrawler.py
@@ -1,8 +1,10 @@
 from travelspider import TravelSpider
 
 class GloriaCrawler(TravelSpider):
-    """docstring for OrangeCrawler."""
-    pass
+    """docstring for GloriaCrawler."""
+    def __init__(self, base_url):
+        super(GloriaCrawler, self).__init__(self, base_url)
+        self.source_site = 3
 
 def main():
     crawler = GloriaCrawler(base_url="https://www.gloriatour.com.tw")
