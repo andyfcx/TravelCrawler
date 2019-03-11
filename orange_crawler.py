a951f6ef57ac35447b438c8fae9239af2d8b8119
Chu-An, Huang <dreamcastle.andy@gmail.com>
2019-03-11 11:52:13 +0800

    [Modify] self.source_site = 1

--- orange_crawler.py
+++ orange_crawler.py
@@ -2,7 +2,9 @@ from travelspider import TravelSpider
 
 class OrangeCrawler(TravelSpider):
     """docstring for OrangeCrawler."""
-    pass
+    def __init__(self, base_url):
+        super(OrangeCrawler, self).__init__(self, base_url)
+        self.source_site = 1
 
 def main():
     crawler = OrangeCrawler(base_url="http://www.orangetour.com.tw")
